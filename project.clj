dac81dd0ddf0c4a8975c51bbc2a515703432b81c
Simon Katz <sk@nomistech.com>
2016-01-05 19:52:53 +0000

    Serve assets with Figwheel's built-in webserver.

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
                  [org.clojure/clojurescript "1.7.170"]
                  [sablono "0.3.6"]]
   :plugins [[lein-figwheel "0.5.0-1"]]
-  :clean-targets [:target-path "out"]
+  :clean-targets [:target-path "out" "resources/public/cljs"]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src"]
                         :figwheel true
-                        :compiler {:main "hello-seymore.core"}}]}
-  :figwheel {:css-dirs ["css"]})
+                        :compiler {:main hello-seymore.core 
+                                   :asset-path "cljs/out"
+                                   :output-to  "resources/public/cljs/main.js"
+                                   :output-dir "resources/public/cljs/out"}}]}
+  :figwheel {:css-dirs ["resources/public/css"]})
